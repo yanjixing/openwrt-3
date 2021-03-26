f0baa00f12cb8e1bea9f878a3156eb50a80ac5f8
yanjixing <78809118+yanjixing@users.noreply.github.com>
2021-03-26 15:21:19 +0800

    Update mt7620.mk

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -404,7 +404,7 @@ TARGET_DEVICES += na930
 define Device/oy-0001
   DTS := OY-0001
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
-  DEVICE_TITLE := BL-845R
+  DEVICE_TITLE := OY-0001
 endef
 TARGET_DEVICES += oy-0001
 
